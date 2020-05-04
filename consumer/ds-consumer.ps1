916285ce43b9209d3149d04b877ebdc00d6f5a8e
ashisa <ashisa78@live.com>
2020-05-04 22:34:17 +0530

    Update ds-consumer.ps1

--- consumer/ds-consumer.ps1
+++ consumer/ds-consumer.ps1
@@ -13,11 +13,12 @@ Write-Host ""
 Write-Host "Initiating Azure Data Share service provisioning..."
 Write-Host ""
 Write-Host "Please follow the instructions below to connect to your Azure subscription."
+Write-Host ""
 Write-Host "Connecting to Azure subscription..."
-Connect-AzAccount
+(Connect-AzAccount -UseDeviceAuthentication) | Out-Null
 
 Write-Host "Creating resource group..." -NoNewline
-New-AzResourceGroup -Name $resourceGroup -Location $location |Out-Null
+(New-AzResourceGroup -Name $resourceGroup -Location $location) |Out-Null
 Write-Host "Done."
 
 Write-Host "Creating data share account..." -NoNewline
@@ -30,19 +31,20 @@ New-AzRoleAssignment -ObjectId $dsAccount.Identity.PrincipalId -RoleDefinitionNa
 Write-Host "Done."
 
 Write-Host "Creating container..." -NoNewline
-New-AzStorageContainer -Container dataset1 -Context $storageAccount.Context |Out-Null
+(New-AzStorageContainer -Container dataset1 -Context $storageAccount.Context) |Out-Null
 Write-Host "Done."
 
 Write-Host "Accepting the invite..." -NoNewline
-New-AzDataShareSubscription -ResourceGroupName $resourceGroup -AccountName $dsaccountName -Name $dsshareName -SourceShareLocation $location -InvitationId $inviteID  |Out-Null
+(New-AzDataShareSubscription -ResourceGroupName $resourceGroup -AccountName $dsaccountName -Name $dsshareName -SourceShareLocation $location -InvitationId $inviteID)  |Out-Null
 Write-Host "Done."
 
 Write-Host "Creating DataSet mapping..." -NoNewline
-New-AzDataShareDataSetMapping -ResourceGroupName $resourceGroup -AccountName $dsaccountName -StorageAccountResourceId $storageAccount.Id -Container $dsContainer -Name $dsshareName -ShareSubscriptionName $dsshareName -DataSetId $datasetId |Out-Null
+(New-AzDataShareDataSetMapping -ResourceGroupName $resourceGroup -AccountName $dsaccountName -StorageAccountResourceId $storageAccount.Id -Container $dsContainer -Name $dsshareName -ShareSubscriptionName $dsshareName -DataSetId $datasetId) |Out-Null
 Write-Host "Done."
 
 Write-Host "Starting initial snapshot in the background..." -NoNewline
-Start-AzDataShareSubscriptionSynchronization -ResourceGroupName $resourceGroup -AccountName $dsaccountName -ShareSubscriptionName $dsshareName -SynchronizationMode FullSync  -AsJob  |Out-Null
+(Start-AzDataShareSubscriptionSynchronization -ResourceGroupName $resourceGroup -AccountName $dsaccountName -ShareSubscriptionName $dsshareName -SynchronizationMode FullSync  -AsJob)  |Out-Null
 Write-Host "Done."
 
-Write-Host "Azure Data Share service provisioning succeeded. Please close this window."
+Write-Host "Azure Data Share service provisioning succeeded."
+Write-Host "You can close this window and visit Azure portal to verify the Azure Data Share service status."
